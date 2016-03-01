e441cf212e7cd75246ca6d458cf3863ca84c8923
Denis Bol <jaddev888@gmail.com>
2016-03-02 00:34:27 +0300

    Урок 7. Layout параметры для View-элементов.

---
+++ AndroidLessons/p0072layoutprop/src/main/java/comdeveloper_megamozg/wix/jaddev888/layoutprop/MainActivity.java
@@ -0,0 +1,13 @@
+package comdeveloper_megamozg.wix.jaddev888.layoutprop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.marginlayout);
+    }
+}
